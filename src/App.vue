dd04c6c029a363bcc980d36d0f0c991f86a8d8fe
Haozhe-Wang-hwan0323 <hwan0323@student.monash.edu>
2025-07-01 15:19:24 +0800

    initial commit to git after finishing Activity 6 of week 2 Studio

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import JSON from './components/JSON.vue';
+</script>
+
+<template>
+  <div class="container mx-auto p-4">
+    <JSON />
+  </div>
+</template>
+
+
+<style>
+body {
+  font-family: Arial, sans-serif;
+  background-color: #f8fafc;
+}
+
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+</style>
